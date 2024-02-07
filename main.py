4df6477eb7c390dd8930c750b39f7574adf9d6b1
Hemant260997 <159246059+Hemant260997@users.noreply.github.com>
2024-02-07 15:08:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src import Gmaps
 love_it_star_it = '''Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/'''
 
 queries = [
-   "web developers in bangalore"
+   "Blue metals in Tamil nadu"
 ]
 
 Gmaps.places(queries, max=5)
